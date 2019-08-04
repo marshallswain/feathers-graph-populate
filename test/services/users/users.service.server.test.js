558ff345d35e2ff1b8590f87391c9440dc2ffc27
Marshall Thompson <marshall@creativeideal.net>
2019-08-04 09:52:26 -0600

    feat: add populate utility

--- test/services/users/users.service.server.test.js
+++ test/services/users/users.service.server.test.js
@@ -3,6 +3,8 @@ const assert = require('assert')
 const { readJsonFileSync } = require('@feathers-plus/test-utils')
 const app = require('../../../src/app')
 const config = require('../../../config/default.json')
+const populateUtil = require('../../../lib/util.populate')
+const userPopulates = require('../../../src/services/users/users.schema').populates
 
 // Determine if environment allows test to mutate existing DB data.
 const env = (config.tests || {}).environmentsAllowingSeedData || []
@@ -43,123 +45,160 @@ describe('Test users/users.service.server.test.js', () => {
     assert.ok(service, 'Registered the service')
   })
 
-  describe('one level deep', () => {
-    it('populates external, by name', async () => {
-      const users = await app.service('users').find({
-        query: {},
-        $populateParams: {
-          name: 'withPosts'
-        },
-        paginate: false,
-        provider: 'socketio'
+  describe('Populate Hook', () => {
+    describe('one level deep', () => {
+      it('populates external, by name', async () => {
+        const users = await app.service('users').find({
+          query: {},
+          $populateParams: {
+            name: 'withPosts'
+          },
+          paginate: false,
+          provider: 'socketio'
+        })
+
+        const user = users[0]
+
+        assert(user.posts.length, 'user has posts')
+        user.posts.forEach(post => {
+          assert.strictEqual(post.authorId, user._id, 'post was added to the correct user')
+          assert(!post.author, 'no author was populated, since we did not request one.')
+          assert(!post.comments, 'no comments were populated, since we did not request any.')
+        })
       })
 
-      const user = users[0]
+      it('populates nothing external, by query', async () => {
+        const users = await app.service('users').find({
+          query: {},
+          $populateParams: {
+            query: {
+              posts: {}
+            }
+          },
+          paginate: false,
+          provider: 'socketio'
+        })
 
-      assert(user.posts.length, 'user has posts')
-      user.posts.forEach(post => {
-        assert.strictEqual(post.authorId, user._id, 'post was added to the correct user')
-        assert(!post.author, 'no author was populated, since we did not request one.')
-        assert(!post.comments, 'no comments were populated, since we did not request any.')
+        const user = users[0]
+
+        assert(!user.posts, 'posts were not populated')
       })
+
+      it('populates internal, by query', async () => {
+        const users = await app.service('users').find({
+          query: {},
+          $populateParams: {
+            query: {
+              posts: {}
+            }
+          },
+          paginate: false
+        })
+
+        const user = users[0]
+
+        assert(user.posts.length, 'user has posts')
+        user.posts.forEach(post => {
+          assert.strictEqual(post.authorId, user._id, 'post was added to the correct user')
+          assert(!post.author, 'no author was populated, since we did not request one.')
+          assert(!post.comments, 'no comments were populated, since we did not request any.')
+        })
+      })
+
     })
+    describe('two levels deep', () => {
+      it('populates external, by name', async () => {
+        const users = await app.service('users').find({
+          query: {},
+          $populateParams: {
+            name: 'postsWithComments'
+          },
+          paginate: false,
+          provider: 'socketio'
+        })
 
-    it('populates nothing external, by query', async () => {
-      const users = await app.service('users').find({
-        query: {},
-        $populateParams: {
-          query: {
-            posts: {}
-          }
-        },
-        paginate: false,
-        provider: 'socketio'
+        const user = users[0]
+
+        assert(user.posts.length, 'user has posts')
+        user.posts.forEach(post => {
+          assert.strictEqual(post.authorId, user._id, 'post was added to the correct user')
+          assert(!post.author, 'no author was populated, since we did not request one.')
+          assert(post.comments.length, 'comments were populated')
+          post.comments.forEach(comment => {
+            assert.strictEqual(post._id, comment.postId, 'the comment was populated on the correct post.')
+          })
+        })
       })
 
-      const user = users[0]
+      it('populates nothing external, by query', async () => {
+        const users = await app.service('users').find({
+          query: {},
+          $populateParams: {
+            query: {
+              posts: {
+                comments: {}
+              }
+            }
+          },
+          paginate: false,
+          provider: 'socketio'
+        })
 
-      assert(!user.posts, 'posts were not populated')
-    })
+        const user = users[0]
 
-    it('populates internal, by query', async () => {
-      const users = await app.service('users').find({
-        query: {},
-        $populateParams: {
-          query: {
-            posts: {}
-          }
-        },
-        paginate: false
+        assert(!user.posts, 'posts were not populated')
       })
 
-      const user = users[0]
+      it('populates internal, by query', async () => {
+        const users = await app.service('users').find({
+          query: {},
+          $populateParams: {
+            query: {
+              posts: {
+                comments: {}
+              }
+            }
+          },
+          paginate: false
+        })
 
-      assert(user.posts.length, 'user has posts')
-      user.posts.forEach(post => {
-        assert.strictEqual(post.authorId, user._id, 'post was added to the correct user')
-        assert(!post.author, 'no author was populated, since we did not request one.')
-        assert(!post.comments, 'no comments were populated, since we did not request any.')
-      })
-    })
+        const user = users[0]
 
-  })
-  describe('two levels deep', () => {
-    it('populates external, by name', async () => {
-      const users = await app.service('users').find({
-        query: {},
-        $populateParams: {
-          name: 'postsWithComments'
-        },
-        paginate: false,
-        provider: 'socketio'
-      })
-
-      const user = users[0]
-
-      assert(user.posts.length, 'user has posts')
-      user.posts.forEach(post => {
-        assert.strictEqual(post.authorId, user._id, 'post was added to the correct user')
-        assert(!post.author, 'no author was populated, since we did not request one.')
-        assert(post.comments.length, 'comments were populated')
-        post.comments.forEach(comment => {
-          assert.strictEqual(post._id, comment.postId, 'the comment was populated on the correct post.')
+        assert(user.posts.length, 'user has posts')
+        user.posts.forEach(post => {
+          assert.strictEqual(post.authorId, user._id, 'post was added to the correct user')
+          assert(!post.author, 'no author was populated, since we did not request one.')
+          assert(post.comments.length, 'comments were populated')
+          post.comments.forEach(comment => {
+            assert.strictEqual(post._id, comment.postId, 'the comment was populated on the correct post.')
+          })
         })
       })
     })
-
-    it('populates nothing external, by query', async () => {
+  })
+  describe('Populate Utility', () => {
+    it('populates on a single record', async () => {
       const users = await app.service('users').find({
-        query: {},
-        $populateParams: {
-          query: {
-            posts: {
-              comments: {}
-            }
-          }
-        },
-        paginate: false,
-        provider: 'socketio'
-      })
-
-      const user = users[0]
-
-      assert(!user.posts, 'posts were not populated')
-    })
-
-    it('populates internal, by query', async () => {
-      const users = await app.service('users').find({
-        query: {},
-        $populateParams: {
-          query: {
-            posts: {
-              comments: {}
-            }
-          }
+        query: {
+          $limit: 1
         },
         paginate: false
       })
-
       const user = users[0]
+
+      await populateUtil(user, {
+        app,
+        params: {
+          $populateParams: {
+            query: {
+              posts: {
+                comments: {}
+              }
+            }
+          }
+        },
+        populates: userPopulates,
+      })
 
       assert(user.posts.length, 'user has posts')
       user.posts.forEach(post => {
