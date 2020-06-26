88ad3ca091ae5b2ac456a34177ce737e8c7f93a9
Marshall <Marshall@creativeideal.net>
2020-06-26 07:40:20 -0600

    lint cleanup

--- src/services/posts/posts.schema.js
+++ src/services/posts/posts.schema.js
@@ -67,7 +67,6 @@ let moduleExports = {
   }
 }
 
-
 module.exports = moduleExports
 
 
--- src/services/posts/posts.sequelize.js
+++ src/services/posts/posts.sequelize.js
@@ -1,15 +1,10 @@
-
 /* eslint quotes: 0 */
 // Defines Sequelize model for service `posts`.
 const merge = require('lodash.merge')
 const Sequelize = require('sequelize')
-// eslint-disable-next-line no-unused-vars
 const DataTypes = Sequelize.DataTypes
 
-
-
 let moduleExports = merge({},
-
   {
     title: {
       type: DataTypes.TEXT
@@ -18,12 +13,6 @@ let moduleExports = merge({},
       type: DataTypes.INTEGER
     }
   },
-
-
 )
 
-
 module.exports = moduleExports
-
-
-
