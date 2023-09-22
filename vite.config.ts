f6e9c06d732f77e335d8c7081f3a9af64408bd38
fratzinger <22286818+fratzinger@users.noreply.github.com>
2023-09-22 14:45:08 +0200

    test: fix vitest provider, c8 -> v8

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   test: {
     globals: true,
     coverage: {
-      provider: 'c8',
+      provider: 'v8',
       reporter: ['text', 'lcov'],
       include: ['src/**/*.{js,ts}'],
       exclude: ['**/*.test.{js,ts}', 'src/types.ts'],
